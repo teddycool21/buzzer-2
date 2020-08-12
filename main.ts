e6db0fa09bc2544a2c9ace4a2f608bb8ec3456ea
teddycool21 <teddylesmana21@gmail.com>
2020-08-12 11:10:43 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+	
+})
